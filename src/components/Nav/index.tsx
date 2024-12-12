4d6fcb7b04b31908cfbc5091031f4c6f03a70354
carvalhoandre <andre_carvalho0@live.com>
2024-12-12 10:38:09 -0300

    fix: z-index of the nav

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,23 +1,40 @@
 'use client';
 import React from 'react';
-
 import classNames from 'classnames';
 
 import useMedia from 'hooks/useMedia';
-
 import MobileButton from './MobileButton';
 import Link from './Link';
+import { navLinks } from 'components/Footer/constant';
 
 const Nav: Component = () => {
 	const [isOpenMenu, setIsOpenMenu] = React.useState(false);
 	const isMobile = useMedia('(max-width: 40rem)');
 
+	const baseClasses = `
+    flex gap-5
+    max-sm:block max-sm:absolute max-sm:top-[70px] max-sm:right-0
+    max-sm:py-0 max-sm:px-4 max-sm:bg-white max-sm:shadow-nav-mobile
+    max-sm:rounded-small transition-all duration-300 z-50
+		z-9
+  `;
+
+	const closedClasses = `
+    max-sm:translate-x-[-10px]
+    max-sm:opacity-0
+    max-sm:pointer-events-none
+  `;
+
+	const openClasses = `
+    max-sm:flex-col
+    translate-x-0
+    pointer-events-auto
+    opacity-100
+  `;
+
 	const navClassNames = classNames(
-		'flex gap-5',
-		'max-sm:block max-sm:absolute max-sm:top-[70px] max-sm:right-0 max-sm:py-0 max-sm:px-4 max-sm:bg-white max-sm:shadow-nav-mobile max-sm:rounded-small max-sm:translate-x-[-10px] max-sm:pointer-events-none',
-		isOpenMenu &&
-			'transition-all duration-300 z-50  translate-x-0 pointer-events-auto max-sm:flex-col',
-		isOpenMenu ? 'opacity-100' : 'max-sm:opacity-0'
+		baseClasses,
+		isMobile ? (isOpenMenu ? openClasses : closedClasses) : null
 	);
 
 	const handleLinkClick = () => {
@@ -31,29 +48,16 @@ const Nav: Component = () => {
 			)}
 
 			<nav className={navClassNames}>
-				<Link
-					href='#about'
-					className='nav-link top-16'
-					onClick={handleLinkClick}
-				>
-					Sobre Mim
-				</Link>
-
-				<Link href='#approaches' className='' onClick={handleLinkClick}>
-					Abordagens
-				</Link>
-
-				<Link
-					href='#testimonials'
-					className='nav-link'
-					onClick={handleLinkClick}
-				>
-					Depoimentos
-				</Link>
-
-				<Link href='#contact' className='nav-link' onClick={handleLinkClick}>
-					Contato
-				</Link>
+				{navLinks.map(({ href, label }, index) => (
+					<Link
+						href={href}
+						className='nav-link'
+						onClick={handleLinkClick}
+						key={index + href}
+					>
+						{label}
+					</Link>
+				))}
 			</nav>
 		</>
 	);
