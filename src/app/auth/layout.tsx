5697a1a23a7116e4115cbf28657f8c2bce7f8714
carvalhoandre <andre_carvalho0@live.com>
2024-12-17 15:34:19 -0300

    fix: update auth layout

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,21 +1,23 @@
-'use client';
-
 import React from 'react';
 
 import { AuthLayoutProps } from './types';
 
-import useMedia from 'hooks/useMedia';
-
 const AuthLayout: Component<AuthLayoutProps> = ({ children }) => {
-	const isMobile = useMedia('(max-width: 40rem)');
-
 	return (
 		<div
-			className={
-				isMobile
-					? 'p-relative min-h-screen'
-					: 'min-h-[calc(100vh-220px)] bg-background flex items-center justify-center'
-			}
+			className='
+				min-h-[calc(100vh-220px)] 
+				bg-background 
+				flex 
+				items-center 
+				justify-center
+				max-sm:relative 
+				max-sm:min-h-screen 
+				max-sm:bg-transparent 
+				max-sm:flex-none 
+				max-sm:items-start 
+				max-sm:justify-start
+			'
 		>
 			<div className='bg-white rounded-md p-8 shadow-md max-w-md w-full'>
 				{children}
