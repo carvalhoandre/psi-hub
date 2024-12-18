468edeaf349355d04c4fed1b444cb3f8eadad98e
carvalhoandre <andre_carvalho0@live.com>
2024-12-18 16:02:22 -0300

    feat: login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,6 +3,9 @@
 import React from 'react';
 import Link from 'next/link';
 
+import Input from 'components/Input';
+import Title from 'components/Title';
+
 const LoginPage: Component = () => {
 	const [email, setEmail] = React.useState('');
 	const [password, setPassword] = React.useState('');
@@ -13,7 +16,47 @@ const LoginPage: Component = () => {
 		console.log('data:', email, password);
 	};
 
-	return <>LOGIN</>;
+	return (
+		<div className='bg-white p-8 rounded shadow-md w-full max-w-sm'>
+			<Title text='Login' />
+
+			<form onSubmit={handleSubmit} className='space-y-4 mt-4 mb-4'>
+				<Input
+					id='email'
+					label='Email'
+					type='email'
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					required
+				/>
+				<Input
+					id='password'
+					label='Senha'
+					type='password'
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					required
+				/>
+				<div>
+					<button
+						type='submit'
+						className='w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary-main hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary'
+					>
+						Entrar
+					</button>
+				</div>
+			</form>
+
+			<div className=' flex flex-col gap-4 mt-4'>
+				<Link
+					href='/forgot-password'
+					className='text-sm text-primary-main hover:underline'
+				>
+					Esqueci minha senha
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default LoginPage;
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,29 +2,57 @@ import React from 'react';
 
 import { InputProps } from './types';
 
+import OptimizedImage from 'components/OptimizedImage';
+
+import EyeIcon from '../../../public/images/icons/eye.svg';
+import EyeSlashIcon from '../../../public/images/icons/eye-slash.svg';
+
 const Input: Component<InputProps> = ({
 	id,
 	label,
 	type = 'text',
 	value,
-	onChange,
 	placeholder = '',
 	required = false,
+	onChange,
 }) => {
+	const [showPassword, setShowPassword] = React.useState(false);
+
+	const togglePasswordVisibility = () => {
+		setShowPassword((prevState) => !prevState);
+	};
+
+	const isPasswordType = type === 'password';
+
 	return (
-		<div className='mb-4'>
+		<div className='mb-4 relative'>
 			<label htmlFor={id} className='block text-sm font-medium text-gray-700'>
 				{label}
 			</label>
-			<input
-				type={type}
-				id={id}
-				value={value}
-				onChange={onChange}
-				placeholder={placeholder}
-				className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-primary focus:border-primary sm:text-sm'
-				required={required}
-			/>
+			<div className='relative'>
+				<input
+					type={isPasswordType && showPassword ? 'text' : type}
+					id={id}
+					value={value}
+					onChange={onChange}
+					placeholder={placeholder}
+					className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-primary focus:border-primary sm:text-sm'
+					required={required}
+				/>
+
+				{isPasswordType && (
+					<button
+						type='button'
+						onClick={togglePasswordVisibility}
+						className='absolute inset-y-0 right-3 flex bg-transparent cursor-pointer items-center text-gray-500 hover:text-gray-700 focus:outline-none'
+					>
+						<OptimizedImage
+							alt='password-icon'
+							src={showPassword ? EyeIcon : EyeSlashIcon}
+						/>
+					</button>
+				)}
+			</div>
 		</div>
 	);
 };
