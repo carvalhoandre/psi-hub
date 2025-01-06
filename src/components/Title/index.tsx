5d0ed447080b07dafc9c2bcc90e63f08eaec3a11
Paulo <pauloh16.pdr@gmail.com>
2025-01-06 20:45:40 -0300

    feat: adjust style of title component

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,11 +1,16 @@
+import classNames from 'classnames';
+
 import { TitleProps } from './types';
 
-const Title: Component<TitleProps> = ({ text }) => {
+const Title: Component<TitleProps> = ({ text, className }) => {
+	const titleClassName = classNames(
+		'font-semibold text-[3rem] leading-[1.125] font-poppins relative z-10',
+		className
+	);
+
 	return (
 		<div className='relative inline-block'>
-			<h1 className='font-semibold text-[3rem] leading-[1.125] font-poppins relative z-10'>
-				{text}
-			</h1>
+			<h1 className={titleClassName}>{text}</h1>
 			<span className='absolute w-6 h-6 bg-primary-main bottom-[5px] left-[-5px] rounded-sm z-0' />
 		</div>
 	);
