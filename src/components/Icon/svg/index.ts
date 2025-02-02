91b29c3fd45acfe5590889488516233666ba6240
Paulo <pauloh16.pdr@gmail.com>
2025-02-02 15:44:04 -0300

    feat: create image upload component

--- src/components/Icon/svg/index.ts
+++ src/components/Icon/svg/index.ts
@@ -1,9 +1,11 @@
 import Check from './check';
 import Close from './close';
+import Upload from './upload';
 
 const SvgList = {
 	check: Check,
 	close: Close,
+	upload: Upload,
 };
 
 export default SvgList;
---
+++ src/components/Icon/svg/upload.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Upload: Component = ({ className }) => {
+	return (
+		<svg
+			width='100%'
+			height='100%'
+			viewBox='0 0 24 24'
+			fill='none'
+			stroke='currentColor'
+			strokeWidth='2'
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			xmlns='http://www.w3.org/2000/svg'
+			className={className}
+		>
+			<path d='M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-7' />
+			<polyline points='17 8 12 3 7 8' />
+			<line x1='12' y1='3' x2='12' y2='15' />
+		</svg>
+	);
+};
+
+export default Upload;
